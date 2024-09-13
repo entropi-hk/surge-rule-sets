644ff821ae4bfe2524e94164158889ae48627796
ainsly <ainsley@entropi.hk>
2024-09-14 03:29:09 +0800

    feat: implement converter for best available wildcard domain sets

---
+++ README.md
@@ -0,0 +1,84 @@
+# Surge DOMAIN-SETs for blocking ads / trackers / malware etc.
+
+A simple script that converts reliable and regularly updated blocklists to a format compatible with [Surge](https://nssurge.com) DOMAIN-SETs.
+
+Surge uses '.' prefixes for wildcard domains, so this simply converts the ready-made wildcard lists from '*.' prefixes to '.'
+
+All credits to [hagezi](https://github.com/hagezi/dns-blocklists/tree/main) and [OISD](https://oisd.nl) and their sources.
+
+README will be updated with more details, but file names are pretty self-explanatory...
+
+In the meantime, check out hagezi's repo and OISD website for more info for more details!
+
+The following files are available:
+
+[hagezi-light.txt](domain-sets/hagezi-light.txt)
+
+[hagezi-multi.txt](domain-sets/hagezi-multi.txt)
+
+[hagezi-pro.txt](domain-sets/hagezi-pro.txt)
+
+[hagezi-pro-mini.txt](domain-sets/hagezi-pro-mini.txt)
+
+[hagezi-pro-plus.txt](domain-sets/hagezi-pro-plus.txt)
+
+[hagezi-pro-plus-mini.txt](domain-sets/hagezi-pro-plus-mini.txt)
+
+[hagezi-ultimate.txt](domain-sets/hagezi-ultimate.txt)
+
+[hagezi-ultimate-mini.txt](domain-sets/hagezi-ultimate-mini.txt)
+
+[hagezi-fake.txt](domain-sets/hagezi-fake.txt)
+
+[hagezi-pop-up-ads.txt](domain-sets/hagezi-pop-up-ads.txt)
+
+[hagezi-tif.txt](domain-sets/hagezi-tif.txt)
+
+[hagezi-tif-medium.txt](domain-sets/hagezi-tif-medium.txt)
+
+[hagezi-tif-mini.txt](domain-sets/hagezi-tif-mini.txt)
+
+[hagezi-doh-vpn-proxy-bypass.txt](domain-sets/hagezi-doh-vpn-proxy-bypass.txt)
+
+[hagezi-doh.txt](domain-sets/hagezi-doh.txt)
+
+[hagezi-no-safe-search.txt](domain-sets/hagezi-no-safe-search.txt)
+
+[hagezi-dynamic-dns.txt](domain-sets/hagezi-dynamic-dns.txt)
+
+[hagezi-badware-hosters.txt](domain-sets/hagezi-badware-hosters.txt)
+
+[hagezi-anti-piracy.txt](domain-sets/hagezi-anti-piracy.txt)
+
+[hagezi-gambling.txt](domain-sets/hagezi-gambling.txt)
+
+[hagezi-gambling-medium.txt](domain-sets/hagezi-gambling-medium.txt)
+
+[hagezi-gambling-mini.txt](domain-sets/hagezi-gambling-mini.txt)
+
+[hagezi-native-trackers-amazon.txt](domain-sets/hagezi-native-trackers-amazon.txt)
+
+[hagezi-native-trackers-apple.txt](domain-sets/hagezi-native-trackers-apple.txt)
+
+[hagezi-native-trackers-huawei.txt](domain-sets/hagezi-native-trackers-huawei.txt)
+
+[hagezi-native-trackers-microsoft.txt](domain-sets/hagezi-native-trackers-microsoft.txt)
+
+[hagezi-native-trackers-tiktok.txt](domain-sets/hagezi-native-trackers-tiktok.txt)
+
+[hagezi-native-trackers-tiktok-aggressive.txt](domain-sets/hagezi-native-trackers-tiktok-aggressive.txt)
+
+[hagezi-native-trackers-lg-web-os.txt](domain-sets/hagezi-native-trackers-lg-web-os.txt)
+
+[hagezi-native-trackers-vivo.txt](domain-sets/hagezi-native-trackers-vivo.txt)
+
+[hagezi-native-trackers-oppo-realme.txt](domain-sets/hagezi-native-trackers-oppo-realme.txt)
+
+[hagezi-native-trackers-oppo-xiaomi.txt](domain-sets/hagezi-native-trackers-oppo-xiaomi.txt)
+
+[oisd-small.txt](domain-sets/oisd-small.txt)
+
+[oisd-big.txt](domain-sets/oisd-big.txt)
+
+[oisd-nsfw.txt](domain-sets/oisd-nsfw.txt)
+
---
+++ index.js
@@ -0,0 +1,65 @@
+import { mkdir, rm, writeFile } from 'node:fs/promises';
+
+const outDir = new URL('./domain-sets/', import.meta.url);
+const lists = [
+  ['hagezi-light', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/light.txt'],
+  ['hagezi-multi', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/multi.txt'],
+  ['hagezi-pro', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.txt'],
+  ['hagezi-pro-mini', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.mini.txt'],
+  ['hagezi-pro-plus', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.txt'],
+  ['hagezi-pro-plus-mini', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.mini.txt'],
+  ['hagezi-ultimate', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate.txt'],
+  ['hagezi-ultimate-mini', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate.mini.txt'],
+  ['hagezi-fake', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/fake.txt'],
+  ['hagezi-pop-up-ads', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/popupads.txt'],
+  ['hagezi-tif', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.txt'],
+  ['hagezi-tif-medium', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.medium.txt'],
+  ['hagezi-tif-mini', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.mini.txt'],
+  ['hagezi-doh-vpn-proxy-bypass', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/doh-vpn-proxy-bypass.txt'],
+  ['hagezi-doh', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/doh.txt'],
+  ['hagezi-no-safe-search', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/nosafesearch.txt'],
+  ['hagezi-dynamic-dns', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/dyndns.txt'],
+  ['hagezi-badware-hosters', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/hoster-onlydomains.txt'],
+  ['hagezi-anti-piracy', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/anti.piracy.txt'],
+  ['hagezi-gambling', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling.txt'],
+  ['hagezi-gambling-medium', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling.medium.txt'],
+  ['hagezi-gambling-mini', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling.mini.txt'],
+  ['hagezi-native-trackers-amazon', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.amazon.txt'],
+  ['hagezi-native-trackers-apple', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.apple.txt'],
+  ['hagezi-native-trackers-huawei', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.huawei.txt'],
+  ['hagezi-native-trackers-microsoft', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.winoffice.txt'],
+  ['hagezi-native-trackers-tiktok', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.tiktok.txt'],
+  ['hagezi-native-trackers-tiktok-aggressive', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.tiktok.extended.txt'],
+  ['hagezi-native-trackers-lg-web-os', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.lgwebos.txt'],
+  ['hagezi-native-trackers-vivo', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.vivo.txt'],
+  ['hagezi-native-trackers-oppo-realme', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.oppo-realme.txt'],
+  ['hagezi-native-trackers-oppo-xiaomi', 'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.xiaomi.txt'],
+  ['oisd-small', 'https://small.oisd.nl/domainswild'],
+  ['oisd-big', 'https://big.oisd.nl/domainswild'],
+  ['oisd-nsfw', 'https://nsfw.oisd.nl/domainswild'],
+];
+
+function getList(url) {
+  return fetch(url).then((response) => response.text())
+}
+
+function transformList(text) {
+  return text.replaceAll(/^[^*].*$/gm, '').replaceAll('*', '');
+}
+
+async function main() {
+  try {
+    await rm(outDir, { force: true, recursive: true });
+    await mkdir(outDir, { recursive: true });
+
+    for (const [name, url] of lists) {
+      const content = transformList(await getList(url));
+      await writeFile(new URL(`${name}.txt`, outDir), content.trim(), { flag: 'w' });
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
